13fe69292120ab28bd14cb1627acb8b75d4767d3
Talha Mansoor <talha131@gmail.com>
2012-08-02 03:36:56 +0500

    Takes user name from command line argument
    http://docs.python.org/library/optparse.html is used to parse the argv

--- twitter-followers-growth.py
+++ twitter-followers-growth.py
@@ -1,9 +1,23 @@
 import requests
+from optparse import OptionParser
 
+# From https://dev.twitter.com/docs/api/1/get/users/show
 url = 'https://api.twitter.com/1/users/show.json'
 
+# Parse command line options
+parser = OptionParser()
+parser.add_option("-u", "--user", 
+                  action="store", 
+                  type="string", 
+                  dest="user",
+                  help="Twitter USER name")
+
+(options, args) = parser.parse_args()
+
+print options.user
+
 parameters = dict(
-  screen_name='talha_131',
+  screen_name=options.user,
   include_entities='true')
 
 response = requests.get(url, params = parameters) 
